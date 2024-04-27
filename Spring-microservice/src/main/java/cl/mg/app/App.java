3e2b132f25c69604f11b2b3fb0ea6e783fc244c7
manuel <manuel@localhost>
2024-04-27 18:23:21 -0400

    [ADD] first commit

---
+++ Spring-microservice/src/main/java/cl/mg/app/App.java
@@ -0,0 +1,13 @@
+package cl.mg.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
